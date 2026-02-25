13a4384847b98c8af6f933880dfb26951262061c
Luccas <lucasgabriel69047@gmail.com>
2026-02-25 00:46:18 +0000

    File changes

--- src/components/progress/StatsModals.jsx
+++ src/components/progress/StatsModals.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { format } from "date-fns";
-import { ptBR } from "date-fns/locale/pt-BR";
+import { ptBR } from "date-fns/locale";
 import { X, Dumbbell, Flame } from "lucide-react";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
